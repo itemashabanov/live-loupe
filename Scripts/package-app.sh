a2ca3c3f8ec2131cd9d23740337394e5d922f44c
Artsiom Shabanau <itemashabanov@users.noreply.github.com>
2026-06-01 19:05:54 +0200

    Bump release version to 0.1.1

--- Scripts/package-app.sh
+++ Scripts/package-app.sh
@@ -67,9 +67,9 @@ cat > "$CONTENTS_PATH/Info.plist" <<PLIST
   <key>LSApplicationCategoryType</key>
   <string>public.app-category.photography</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.1.0</string>
+  <string>0.1.1</string>
   <key>CFBundleVersion</key>
-  <string>1</string>
+  <string>2</string>
   <key>LSMinimumSystemVersion</key>
   <string>14.0</string>
   <key>NSHighResolutionCapable</key>
